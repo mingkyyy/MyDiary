b330e8315c94d239541130dad3346945d64cbd05
mingkyy <alsrus1227@naver.com>
2023-01-02 17:01:01 +0900

    Initial commit

---
+++ src/main/java/com/mingkyy/MyDiary/MyDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mingkyy.MyDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDiaryApplication.class, args);
+	}
+
+}
