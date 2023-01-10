45c69cc6281769950bc839e6bf6ce13fbaed0525
mingkyy <alsrus1227@naver.com>
2023-01-10 20:25:43 +0900

    spring security 설정

---
+++ src/main/java/com/mingkyy/MyDiary/configuration/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.mingkyy.MyDiary.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/")
+                .permitAll()
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
+
+        return http.build();
+    }
+}
